2880991ff7d25936e12fc97c46af2042f8368ec0
Anamya Vats <121956516+Anamya01@users.noreply.github.com>
2024-09-05 20:07:29 +0530

    Update Saved.jsx

--- src/components/SaveShows/Saved.jsx
+++ src/components/SaveShows/Saved.jsx
@@ -7,15 +7,19 @@ import { useState, useEffect } from 'react';
 import { UserAuth } from '../../context/AuthContext';
 import { db } from '../../firebase';
 import { updateDoc, doc, onSnapshot } from 'firebase/firestore';
+import Cards from '../Cards/Cards';
+
 
 function Saved() {
-  const [Lists, setList] = useState([{}]);
+  const [Lists, setList] = useState([]);
   const { user } = UserAuth();
 
 
   useEffect(() => {
-    onSnapshot(doc(db, 'users', `${user?.email}`), (doc) => {
-      setList(doc.data()?.savedShows);
+    onSnapshot(doc(db, 'users', `${user?.email}`), async (doc) => {
+      const meta = await doc.data()?.savedShows;
+      setList(meta);
+      console.log(Lists);
     });
   }, [user?.email]);
 
@@ -32,12 +36,9 @@ function Saved() {
       }
   }
 
-
   return (
     <div className='container1'>
-    { Lists === undefined ? (<p>Loading</p>) :
-      Lists.map(items => <SavedShows param={items} />)
-    }
+    { Lists == [] ? (<p></p>) : (Lists.map(items => <Cards param={items.data.param} media = {items.type} />))}
   </div>
   )
 }
